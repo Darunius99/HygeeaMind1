77f80fa74cf934e9b99a8a198d9375510e19dd9b
Darunius99 <darius-dorin.popovici@student.upt.ro>
2025-06-03 11:23:45 +0300

    Add project files.

---
+++ ViewModels/HomeViewModel.cs
@@ -0,0 +1,16 @@
+﻿using HygeeaMind.Models; // Asigură-te că modelele sunt accesibile
+using System.Collections.Generic; // Pentru IEnumerable
+
+namespace HygeeaMind.ViewModels
+{
+    public class HomeViewModel
+    {
+        // Proprietăți pentru listele de date pe care le vom afișa pe pagina Home
+        public required IEnumerable<Article> Articles { get; set; }
+        public required IEnumerable<Specialist> Specialists { get; set; }
+
+        // Proprietate pentru a reține stringul de căutare
+        // Utilizată pentru a pre-popula câmpul de căutare după o căutare
+        public required string SearchString { get; set; }
+    }
+}
